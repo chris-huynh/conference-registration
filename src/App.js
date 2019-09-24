2920fb7859aebaac5ad8c1576da0c0377a882756
chris-huynh <41095524+chris-huynh@users.noreply.github.com>
2019-09-23 21:31:51 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LandingPage from "./pages/LandingPage";
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload. <br/>
+                    Howdy y'all
+                </p>
+
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path="/landing" component={LandingPage}/>
+                    </Switch>
+                </BrowserRouter>
+
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+class LandingPage extends Component{
+    render() {
+        return(<p>This is the landing page component</p>)
+    }
+}
+
+export default LandingPage
