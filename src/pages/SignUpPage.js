3ac94265e5be3679d9eef58621ff8a3b6bda82b7
chris-huynh <41095524+chris-huynh@users.noreply.github.com>
2019-09-26 21:49:31 -0400

    Add Grid layout to registration form

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -3,22 +3,33 @@ import Button from '@material-ui/core/Button';
 import TextField from "@material-ui/core/TextField";
 import {Container} from "@material-ui/core";
 
+import Grid from "@material-ui/core/Grid";
 import '../styles/SignUpPage.css'
 
 const SignUpPage = () => {
-  return (
-    <div>
-      This is the register component
-      <Container maxWidth="md">
-        <TextField autoFocus="true" required="true" label="Email Address" autoComplete="true"/>
-        <br/>
-        <TextField required="true" label="Password" type="password"/>
-        <br/>
-        <Button variant="outlined" color="primary">
-          Submit
-        </Button>
-      </Container>
-    </div>)
+    return (
+        <div>
+            This is the register component
+            <Container maxWidth='md'>
+                <Grid container spacing={3}>
+                    <Grid item xs={3} />
+                    <Grid item xs={6}>
+                        <TextField variant="filled" autoFocus="true" required="true" label="Email Address" autoComplete="true" margin="normal" fullWidth/>
+                    </Grid>
+                    <Grid item xs={3} />
+                    <Grid item xs={3} />
+                    <Grid item xs={6}>
+                        <TextField variant="filled" required="true" label="Password" type="password" margin="normal" fullWidth/>
+                    </Grid>
+                    <Grid item xs={3} />
+                    <Grid item xs={12}>
+                        <Button variant="outlined" color="primary">
+                            Submit
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>)
 };
 
 export default SignUpPage
